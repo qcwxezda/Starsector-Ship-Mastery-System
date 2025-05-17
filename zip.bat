2584bef9019277ea19bb828a0e42f617584296a3
qcwxezda <hy_pi_corism@live.com>
2025-05-17 10:50:05 -0400

    add batch command to quickly make zip

---
+++ zip.bat
@@ -0,0 +1,6 @@
+@echo off
+set "current_dir=%cd%"
+for %%f in ("%CD%") do set "copy_dir=%%~nxf"
+robocopy . "%copy_dir%" /E /JOB:zip /xd "%copy_dir%"
+"C:\Program Files\7-Zip\7z.exe" a -tzip "%copy_dir%" "%copy_dir%\*" -r
+rmdir "%copy_dir%" /S /Q
